33292752316d50488ef3f1e48bead377f5e0366f
KoblizekXD <janprokupek@centrum.cz>
2024-12-02 09:05:38 +0100

    Day 2 Part 2

--- src/main/kotlin/lol/koblizek/aoc/Day2.kt
+++ src/main/kotlin/lol/koblizek/aoc/Day2.kt
@@ -1,30 +1,47 @@
 package lol.koblizek.aoc
 
 import lol.koblizek.aoc.util.readInput
+import sun.tools.jconsole.VariableGridLayout
 import kotlin.math.abs
+
+fun checkIfSafe(l: List<Int>): Boolean {
+    val isIncreasing = l[1] - l[0] > 0
+    var canBeSafe = true
+    
+    l.forEachIndexed { i, v ->
+        if (i == l.size - 1) return@forEachIndexed
+        
+        val diff = l[i + 1] - v
+        val absDiff = abs(diff)
+        
+        if (isIncreasing && diff < 0 || !isIncreasing && diff > 0
+            || absDiff < 1 || absDiff > 3) {
+            canBeSafe = false
+            return@forEachIndexed
+        }
+    }
+    
+    return canBeSafe
+}
 
 fun main() {
     val lines = readInput(2, false).map { it.split(" ").map { it.toInt() } }
     
     var safe = 0;
-    lines.forEach { l ->
-        val isIncreasing = l[1] - l[0] > 0
-        var canBeSafe = true
-        var freeLevel = false
-        for (i in 0..(l.size - 2)) {
-            val diff = l[i + 1] - l[i]
-            if (diff > 0 != isIncreasing
-                || abs(diff) == 0 || abs(diff) > 3) {
-                if (!freeLevel) {
-                    freeLevel = true
-                } else {
-                    canBeSafe = false
-                    break
+    lines.forEachIndexed { i, l ->
+        val x = checkIfSafe(l)
+        if (x) safe++
+        else {
+            run {
+                l.forEachIndexed { i, v ->
+                    val list = l.toMutableList()
+                    list.removeAt(i)
+                    if (checkIfSafe(list)) {
+                        safe++
+                        return@run
+                    }
                 }
             }
-        }
-        if (canBeSafe) {
-            safe++
         }
     }
     println(safe)
