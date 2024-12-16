abd779cf5f1486aab11bcee7f39b66c3f2e71115
KoblizekXD <janprokupek@centrum.cz>
2024-12-16 15:10:18 +0100

    Day 16 p2

--- src/main/kotlin/lol/koblizek/aoc/Day16.kt
+++ src/main/kotlin/lol/koblizek/aoc/Day16.kt
@@ -2,31 +2,76 @@ package lol.koblizek.aoc
 
 import lol.koblizek.aoc.util.Direction
 import lol.koblizek.aoc.util.Grid
+import lol.koblizek.aoc.util.print
 import lol.koblizek.aoc.util.println
+import java.io.Serializable
 import java.util.PriorityQueue
 
-fun findP1(grid: Grid, root: Grid.Point, target: Grid.Point): Int {
-    val queue = PriorityQueue<Triple<Grid.Point, Direction, Int>>(compareBy { it.third })
-    queue.add(Triple(root, Direction.EAST, 0))
+data class Quadruple<out A, out B, out C, out D>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D
+) : Serializable {
+    override fun toString(): String = "($first, $second, $third, $fourth)"
+}
+
+fun findP2(grid: Grid, root: Grid.Point, target: Grid.Point): List<Quadruple<Grid.Point, Direction, Int, MutableList<Grid.Point>>?> {
+    val queue = PriorityQueue<Quadruple<Grid.Point, Direction, Int, MutableList<Grid.Point>>>(compareBy { it.third })
+    queue.add(Quadruple(root, Direction.EAST, 0, mutableListOf()))
+    val seen = hashMapOf<Pair<Grid.Point, Direction>, Int>()
+    val idkMap = mutableListOf<Quadruple<Grid.Point, Direction, Int, MutableList<Grid.Point>>?>()
+    while (queue.isNotEmpty()) {
+        val res = queue.poll()
+        val (point, direction, distance, previous) = res
+        if (point == target) {
+            idkMap.add(res)
+            continue
+        }
+        if (seen[point to direction] != null && seen[point to direction]!! < distance) continue
+        seen[point to direction] = distance
+        if (grid[point + direction.toPoint()] != '#') {
+            val new = previous.toMutableList()
+            new.add(point + direction.toPoint())
+            queue.add(Quadruple(point + direction.toPoint(), direction, distance + 1, new))
+        }
+        queue.add(Quadruple(point, direction + Direction.EAST, distance + 1000, previous))
+        queue.add(Quadruple(point, direction + Direction.WEST, distance + 1000, previous))
+    }
+    return idkMap
+}
+
+fun findP1(grid: Grid, root: Grid.Point, target: Grid.Point): Any {
+    val queue = PriorityQueue<Quadruple<Grid.Point, Direction, Int, MutableList<Grid.Point>>>(compareBy { it.third })
+    queue.add(Quadruple(root, Direction.EAST, 0, mutableListOf()))
     val seen = hashMapOf<Pair<Grid.Point, Direction>, Int>()
     while (queue.isNotEmpty()) {
-        val (point, direction, distance) = queue.poll()
+        val (point, direction, distance, previous) = queue.poll()
         if (point == target) {
             return distance
         }
         if (seen[point to direction] != null && seen[point to direction]!! < distance) continue
         seen[point to direction] = distance
         if (grid[point + direction.toPoint()] != '#') {
-            queue.add(Triple(point + direction.toPoint(), direction, distance + 1))
+            val new = previous.toMutableList()
+            new.add(point + direction.toPoint())
+            queue.add(Quadruple(point + direction.toPoint(), direction, distance + 1, new))
         }
-        queue.add(Triple(point, direction + Direction.EAST, distance + 1000))
-        queue.add(Triple(point, direction + Direction.WEST, distance + 1000))
+        queue.add(Quadruple(point, direction + Direction.EAST, distance + 1000, previous))
+        queue.add(Quadruple(point, direction + Direction.WEST, distance + 1000, previous))
     }
     return 0
 }
 
 fun main() {
     val grid = Grid(16, false)
-    findP1(grid, grid.find { it == 'S' }!!, grid.find { it == 'E' }!!)
-        .println()
+    val x = findP2(grid, grid.find { it == 'S' }!!, grid.find { it == 'E' }!!)
+    val min = x.minBy { it!!.third }
+    x.filter { it!!.third == min!!.third }.forEach { 
+        it!!.fourth.forEach { 
+            grid[it] = 'O'
+        }
+    }
+    grid.print()
+    grid.count { it == 'O' || it == 'S' || it == 'E' }.println()
 }
